76bf3d917fc67181c1a72e97174495051bbae794
Bruno Grande <bruno.grande@sagebase.org>
2022-06-15 00:32:14 -0700

    Reorganize module imports

--- sagetasks/cavatica.py
+++ sagetasks/cavatica.py
@@ -4,9 +4,8 @@ Adapted from code that Tom Yu authored:
 https://github.com/include-dcc/synapse-cavatica/blob/main/scripts/rnaseq_flow.py
 """
 
-import sevenbridges as sbg
-
 from prefect import Task
+import sevenbridges as sbg
 
 
 class CavaticaBaseTask(Task):
