117ec072cceb77db5d99ae5c46d0dbd20c7fe3e6
thomasyu888 <thomas.yu@sagebase.org>
2022-08-12 14:12:39 +0800

    Add example k8 operator

---
+++ airflow_dags/k8_operator.py
@@ -0,0 +1,18 @@
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+    KubernetesPodOperator,
+)
+with DAG(
+    dag_id='k8_test'
+) as dag:
+    k = KubernetesPodOperator(
+        name="hello-dry-run",
+        image="debian",
+        cmds=["bash", "-cx"],
+        arguments=["echo", "10"],
+        labels={"foo": "bar"},
+        task_id="dry_run_demo",
+        do_xcom_push=True,
+    )
+
+if __name__ == "__main__":
+    dag.cli()
