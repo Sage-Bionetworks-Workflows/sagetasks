e6892871fb5dbfe5864f12f27f18b932b4d5dec7
thomasyu888 <thomas.yu@sagebase.org>
2022-08-12 14:45:01 +0800

    Test secrets

--- airflow_dags/k8_operator.py
+++ airflow_dags/k8_operator.py
@@ -4,8 +4,12 @@ import pendulum
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.kubernetes.secret import Secret
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 
+SYNAPSE_AUTH_TOKEN = Secret(
+   deploy_type="env", deploy_target="SYNAPSE_AUTH_TOKEN", secret="tyu-syn-pat", key="SYNAPSE_AUTH_TOKEN"
+)
 
 with DAG(
     dag_id='k8_test',
@@ -19,13 +23,14 @@ with DAG(
     k = KubernetesPodOperator(
         namespace='airflow',
         name="hello-dry-run",
-        image="debian",
-        cmds=["bash", "-cx"],
-        arguments=["echo", "10"],
+        image="sagebionetworks/synapsepythonclient",
+        cmds=["synapse", "login"],
+        # arguments=["echo", "$SYNAPSE_AUTH_TOKEN"],
         labels={"foo": "bar"},
         task_id="dry_run_demo",
         # in_cluster=True,
-        is_delete_operator_pod=True
+        is_delete_operator_pod=True,
+        secrets=[SYNAPSE_AUTH_TOKEN]
         # do_xcom_push=True,
     )
 
